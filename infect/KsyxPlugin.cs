ed8a7a9f675581d01979243aaaa3f13663b81086
Phongx2 <n1187489965@163.com>
2026-07-30 16:53:42 +0800

    Update KsyxPlugin.cs

--- infect/KsyxPlugin.cs
+++ infect/KsyxPlugin.cs
@@ -6,6 +6,18 @@ public class HelloPlugin : DeadworksPluginBase
 {
     public override string Name => "Hello";
 
+    // ========== 必须添加这两个方法 ==========
+    public override void OnLoad(bool isReload)
+    {
+        Console.WriteLine($"[{Name}] Loaded! (reload={isReload})");
+    }
+
+    public override void OnUnload()
+    {
+        Console.WriteLine($"[{Name}] Unloaded!");
+    }
+    // ========== 添加结束 ==========
+
     [Command("hello", Description = "Show a welcome message")]
     public void CmdHello(CCitadelPlayerController caller)
     {
