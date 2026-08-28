f4efbfcc2475350dfa9d42ecd345cce687ddb8ea
Phongx2 <n1187489965@163.com>
2026-08-28 09:52:07 +0800

    Update teamdeadmatch2.cs

--- deadmatch2/teamdeadmatch2.cs
+++ deadmatch2/teamdeadmatch2.cs
@@ -4,7 +4,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using DeadworksManaged.Api;
 
-namespace DeathmatchPlugin;e
+namespace DeathmatchPlugin;
 
 public class SpawnPoint {
     [JsonPropertyName("pos")]
